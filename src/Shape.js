a29299a27a8898e13aa5bf39820f274b960e6fa2
benjambo13 <benjambo13@gmail.com>
2024-07-04 17:37:39 +0100

    final

--- src/Shape.js
+++ src/Shape.js
@@ -3,20 +3,27 @@ import './Shape.css'
 
 export const Shape = ({ data }) => {
   const [activeSquares, setActiveSquares] = useState([])
+  const [isUnloading, setIsUnloading] = useState(false)
 
   const flatData = useMemo(() => data.flat(), [data])
-  const visibleSquareCount = useMemo(() => flatData.reduce((acc, nextItem) => acc + nextItem, 0), [flatData])
+  const visibleSquareCount = useMemo(() => flatData.reduce((acc, nextItem) => acc + nextItem), [flatData])
 
   const isSquareVisible = item => item === 1
 
   const isSquareActive = index => activeSquares.includes(index)
 
-  const onSquareClick = (item, index) => {
-    if (!isSquareVisible(item) || isSquareActive(index)) return
+  const onSquareClick = event => {
+    const itemString = event.target.getAttribute('data-item')
+    if (!itemString) return
+  
+    const item = parseInt(event.target.getAttribute('data-item'))
+    const index = parseInt(event.target.getAttribute('data-index'))
+    if (!isSquareVisible(item) || isSquareActive(index) || isUnloading) return
 
     setActiveSquares(prevActiveSquares => [...prevActiveSquares, index])
 
     if (activeSquares.length + 1 === visibleSquareCount) {
+      setIsUnloading(true)
       unloadActiveSquares(visibleSquareCount)
     }
   }
@@ -26,16 +33,24 @@ export const Shape = ({ data }) => {
       setActiveSquares(prevActiveSquares => prevActiveSquares.slice(1))
 
       setTimeout(() => unloadActiveSquares(numToDelete - 1), 750)
+    } else {
+      setIsUnloading(false)
     }
   }
 
   return (
-    <div className='shape'>
+    <div className='shape' onClick={onSquareClick}>
       {flatData.map((item, index) => (
         <div
           key={index}
-          className={`square ${isSquareVisible(item) && 'square-visible'} ${isSquareActive(index) && 'square-active'}`}
-          onClick={() => onSquareClick(item, index)}
+          className={`
+            square
+            ${isSquareVisible(item) && 'square-visible'}
+            ${isSquareActive(index) && 'square-active'}
+            ${isUnloading && 'square-unloading'}
+          `}
+          data-item={item}
+          data-index={index}
         />
       ))}
     </div>
